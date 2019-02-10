23b64b4ee6ef30333544b1f898d8b69e90fa67ca
aziz <aziz.icheddadene@gmail.com>
2019-02-10 18:01:26 +0100

    Adding POJOs. Closes #2.

---
+++ src/main/java/com/springframwork/petclinicspring/model/Owner.java
@@ -0,0 +1,4 @@
+package com.springframwork.petclinicspring.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/com/springframwork/petclinicspring/model/Vet.java
@@ -0,0 +1,4 @@
+package com.springframwork.petclinicspring.model;
+
+public class Vet extends Person {
+}
