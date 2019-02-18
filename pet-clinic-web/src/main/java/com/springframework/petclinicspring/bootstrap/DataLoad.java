5c1b325c0b595f656385ef0ee24518649d1cf1e7
aziz <aziz.icheddadene@gmail.com>
2019-02-18 20:57:51 +0100

    Adding bootstrap for load data on startup. Closes #14

--- pet-clinic-web/src/main/java/com/springframework/petclinicspring/bootstrap/DataLoad.java
+++ pet-clinic-web/src/main/java/com/springframework/petclinicspring/bootstrap/DataLoad.java
@@ -26,13 +26,11 @@ public class DataLoad implements CommandLineRunner {
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
-
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glenanne");
-
         ownerService.save(owner2);
 
         System.out.println("Loaded Owners....");
@@ -40,13 +38,11 @@ public class DataLoad implements CommandLineRunner {
         Vet vet1 = new Vet();
         vet1.setFirstName("Sam");
         vet1.setLastName("Axe");
-
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
         vet2.setFirstName("Jessie");
         vet2.setLastName("Porter");
-
         vetService.save(vet2);
 
         System.out.println("Loaded Vets....");
