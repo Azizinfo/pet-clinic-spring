763f38dfd87e7969b0c5e70d62cb70b06c34ce8a
aziz <aziz.icheddadene@gmail.com>
2019-02-24 18:27:28 +0100

    added PetTypeService. Closes #29

---
+++ pet-clinic-data/src/main/java/com/springframework/petclinicspring/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.springframework.petclinicspring.services;
+
+import com.springframework.petclinicspring.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
