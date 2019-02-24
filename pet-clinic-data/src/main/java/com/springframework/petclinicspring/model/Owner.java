2654bfc8093aaa09dd0b04328e30f71dbd24aead
aziz <aziz.icheddadene@gmail.com>
2019-02-24 18:10:17 +0100

    added propeties to Owner. Closes #28

--- pet-clinic-data/src/main/java/com/springframework/petclinicspring/model/Owner.java
+++ pet-clinic-data/src/main/java/com/springframework/petclinicspring/model/Owner.java
@@ -4,7 +4,34 @@ import java.util.Set;
 
 public class Owner extends Person {
 
-    Set<Pet> pets;
+    private String adresse;
+    private String city;
+    private String telephone;
+    private Set<Pet> pets;
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
 
     public Set<Pet> getPets() {
         return pets;
