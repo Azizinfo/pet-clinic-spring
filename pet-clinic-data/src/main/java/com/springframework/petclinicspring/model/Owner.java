4c514c75b3f364becddc58b7a94ab39914a5dcba
aziz <aziz.icheddadene@gmail.com>
2019-02-27 19:39:13 +0100

    Updated Owner entity with JPA Entity mappings. Closes #36

--- pet-clinic-data/src/main/java/com/springframework/petclinicspring/model/Owner.java
+++ pet-clinic-data/src/main/java/com/springframework/petclinicspring/model/Owner.java
@@ -1,13 +1,19 @@
 package com.springframework.petclinicspring.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "owners")
 public class Owner extends Person {
-
+    @Column(name = "adresse")
     private String adresse;
+    @Column(name = "city")
     private String city;
+    @Column(name = "telephone")
     private String telephone;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
 
     public String getAdresse() {
