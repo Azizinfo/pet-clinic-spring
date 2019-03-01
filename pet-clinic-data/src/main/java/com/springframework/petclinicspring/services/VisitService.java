ed599c845727e993621f1ecde67efdd06fd482ec
aziz <aziz.icheddadene@gmail.com>
2019-03-01 19:53:52 +0100

    Implement map visit service. Closes #46

---
+++ pet-clinic-data/src/main/java/com/springframework/petclinicspring/services/VisitService.java
@@ -0,0 +1,7 @@
+package com.springframework.petclinicspring.services;
+
+import com.springframework.petclinicspring.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long>{
+
+}
